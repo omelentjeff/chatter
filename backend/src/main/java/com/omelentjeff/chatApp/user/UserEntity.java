4b3d032d5622edffe19408ffd0aef59e4ad27ccd
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-10-11 15:23:31 +0300

    BACKEND feat: implement getAuthorities -method in UserEntity

--- backend/src/main/java/com/omelentjeff/chatApp/user/UserEntity.java
+++ backend/src/main/java/com/omelentjeff/chatApp/user/UserEntity.java
@@ -6,9 +6,11 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.List;
 
 @Data
 @Builder
@@ -30,13 +32,13 @@ public class UserEntity implements UserDetails {
     private String username;
 
     private String password;
-    
+
     @Enumerated(EnumType.STRING)
     private Role role;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return List.of(new SimpleGrantedAuthority(role.name()));
     }
 
     @Override
