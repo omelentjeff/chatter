7d4c20bb661610ce54c2968407938308fd55d68b
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-10-11 14:43:18 +0300

    BACKEND feat: create User model and properties

---
+++ backend/src/main/java/com/omelentjeff/chatApp/user/User.java
@@ -0,0 +1,20 @@
+package com.omelentjeff.chatApp.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    private Integer id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String username;
+    private String password;
+}
