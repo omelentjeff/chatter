b8693d4e378279e829514730bcd847b3746a032a
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-10-14 16:13:17 +0300

    BACKEND feat: create custom method findByUsername

--- backend/src/main/java/com/omelentjeff/chatApp/repositories/UserRepository.java
+++ backend/src/main/java/com/omelentjeff/chatApp/repositories/UserRepository.java
@@ -2,6 +2,12 @@ package com.omelentjeff.chatApp.repositories;
 
 import com.omelentjeff.chatApp.models.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+
+    Optional<UserEntity> findByUsername(String username);
 }
