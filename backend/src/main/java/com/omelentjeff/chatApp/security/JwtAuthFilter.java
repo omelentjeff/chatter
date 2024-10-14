3fb6fe29515fe32449b24e362fd73ee77304f90c
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-10-14 17:01:24 +0300

    BACKEND feat: implement JwtAuthFilter method logic for no token found

--- backend/src/main/java/com/omelentjeff/chatApp/security/JwtAuthFilter.java
+++ backend/src/main/java/com/omelentjeff/chatApp/security/JwtAuthFilter.java
@@ -5,7 +5,6 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -20,6 +19,16 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     protected void doFilterInternal(@NonNull HttpServletRequest request,
                                     @NonNull HttpServletResponse response,
                                     @NonNull FilterChain filterChain) throws ServletException, IOException {
+
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String userEmail;
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        jwt = authHeader.substring(7);
 
 
     }
