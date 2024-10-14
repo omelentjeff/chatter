ea96b057cee0d952dc01d58d763d2c27ef534b42
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-10-14 16:38:24 +0300

    BACKEND feat: add annotations for JwtAuthFilter

--- backend/src/main/java/com/omelentjeff/chatApp/models/UserEntity.java
+++ backend/src/main/java/com/omelentjeff/chatApp/models/UserEntity.java
@@ -22,7 +22,7 @@ public class UserEntity implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-
+    
     private String firstname;
 
     private String lastname;
--- backend/src/main/java/com/omelentjeff/chatApp/security/JwtAuthFilter.java
+++ backend/src/main/java/com/omelentjeff/chatApp/security/JwtAuthFilter.java
@@ -1,4 +1,26 @@
 package com.omelentjeff.chatApp.security;
 
-public class JwtAuthFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain) throws ServletException, IOException {
+
+
+    }
 }
