7036700f744fba9a763fd7c08277841321314106
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-10-14 17:45:28 +0300

    BACKEND feat: implement extractAllClaims method

--- backend/src/main/java/com/omelentjeff/chatApp/services/JwtService.java
+++ backend/src/main/java/com/omelentjeff/chatApp/services/JwtService.java
@@ -1,7 +1,31 @@
 package com.omelentjeff.chatApp.services;
 
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
 
 @Service
 public class JwtService {
+
+    private static final String SECRET_KEY = "SECRET";
+    public String extractUsername(String jwt) {
+        return null;
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getSigninKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+
+    }
+
+    private SecretKey getSigninKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
 }
