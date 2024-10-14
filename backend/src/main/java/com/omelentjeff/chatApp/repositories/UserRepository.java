70435e4be4a56439173164bdc74ba33a7102e8da
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-10-14 16:13:38 +0300

    BACKEND feat: create custom method findByEmail

--- backend/src/main/java/com/omelentjeff/chatApp/repositories/UserRepository.java
+++ backend/src/main/java/com/omelentjeff/chatApp/repositories/UserRepository.java
@@ -10,4 +10,5 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
 
     Optional<UserEntity> findByUsername(String username);
+    Optional<UserEntity> findByEmail(String email);
 }
