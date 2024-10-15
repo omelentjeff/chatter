419067255b5b1a677cb63a1c623eefaa0e92f129
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-10-15 14:48:25 +0300

    BACKEND feat: implement generateToken method

--- backend/src/main/java/com/omelentjeff/chatApp/services/JwtService.java
+++ backend/src/main/java/com/omelentjeff/chatApp/services/JwtService.java
@@ -3,9 +3,12 @@ package com.omelentjeff.chatApp.services;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.Map;
 import java.util.function.Function;
 
 @Service
@@ -19,6 +22,20 @@ public class JwtService {
     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
+    }
+
+    public String generateToken(
+            Map<String, Object> extraClaims,
+            UserDetails userDetails
+    ) {
+        return Jwts
+                .builder()
+                .claims(extraClaims)
+                .subject(userDetails.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24))
+                .signWith(getSigningKey())
+                .compact();
     }
 
     private Claims extractAllClaims(String token) {
