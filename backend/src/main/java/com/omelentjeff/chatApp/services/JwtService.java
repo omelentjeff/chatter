5173859d9804191ff0dc0c520c18f1721ed37b83
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-10-15 15:15:07 +0300

    BACKEND feat: create method for checking token expiration

--- backend/src/main/java/com/omelentjeff/chatApp/services/JwtService.java
+++ backend/src/main/java/com/omelentjeff/chatApp/services/JwtService.java
@@ -49,6 +49,10 @@ public class JwtService {
         return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
     }
 
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
     private Claims extractAllClaims(String token) {
         return Jwts.parser()
                 .verifyWith(getSigningKey())
