2ec19f8870acad20e0b8610677c8454080c9f845
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-10-17 17:18:14 +0300

    BACKEND feat: inject UserRepository into AuthenticationService

--- backend/src/main/java/com/omelentjeff/chatApp/auth/AuthenticationService.java
+++ backend/src/main/java/com/omelentjeff/chatApp/auth/AuthenticationService.java
@@ -1,11 +1,15 @@
 package com.omelentjeff.chatApp.auth;
 
+import com.omelentjeff.chatApp.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
+
+    private final UserRepository repository;
+
     public AuthenticationResponse register(RegisterRequest request) {
         return null;
     }
