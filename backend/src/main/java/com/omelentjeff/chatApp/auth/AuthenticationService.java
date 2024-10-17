40f4dac0524cfe2de70f8227cdab6421597a62e0
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-10-17 17:31:50 +0300

    BACKEND feat: implement AuthenticationService register method

--- backend/src/main/java/com/omelentjeff/chatApp/auth/AuthenticationService.java
+++ backend/src/main/java/com/omelentjeff/chatApp/auth/AuthenticationService.java
@@ -1,6 +1,9 @@
 package com.omelentjeff.chatApp.auth;
 
+import com.omelentjeff.chatApp.models.Role;
+import com.omelentjeff.chatApp.models.UserEntity;
 import com.omelentjeff.chatApp.repositories.UserRepository;
+import com.omelentjeff.chatApp.services.JwtService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -11,9 +14,24 @@ public class AuthenticationService {
 
     private final UserRepository repository;
     private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
 
     public AuthenticationResponse register(RegisterRequest request) {
-        return null;
+        // TODO checkkaa onko käyttäjätiedot jo olemassa, jos on niin heitä usernameAlreadyInUse
+
+        var user = UserEntity.builder()
+                .username(request.getUsername())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+
+        repository.save(user);
+
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
