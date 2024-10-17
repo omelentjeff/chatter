56a4505136cd16049a611fdec741b304c90bba5b
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-10-17 17:03:57 +0300

    BACKEND feat: inject AuthenticationService into AuthenticationController

--- backend/src/main/java/com/omelentjeff/chatApp/auth/AuthenticationController.java
+++ backend/src/main/java/com/omelentjeff/chatApp/auth/AuthenticationController.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthenticationController {
 
+    private final AuthenticationService service;
+
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(
             @RequestBody RegisterRequest request
