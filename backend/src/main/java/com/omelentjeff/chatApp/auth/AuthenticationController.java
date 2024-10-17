b294fd96a4e1896edcf49798d466f51760962c41
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-10-17 17:01:52 +0300

    BACKEND refactor: rename AuthController -> AuthenticationController

--- backend/src/main/java/com/omelentjeff/chatApp/auth/AuthenticationController.java
+++ backend/src/main/java/com/omelentjeff/chatApp/auth/AuthenticationController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
-public class AuthController {
+public class AuthenticationController {
 
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(
