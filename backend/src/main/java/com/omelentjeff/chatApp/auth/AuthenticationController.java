fa074f0fb13e876c5a9460dd4fdb5cdb5ab673b1
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-10-17 17:09:49 +0300

    BACKEND feat: implement controller's authenticate method

--- backend/src/main/java/com/omelentjeff/chatApp/auth/AuthenticationController.java
+++ backend/src/main/java/com/omelentjeff/chatApp/auth/AuthenticationController.java
@@ -27,6 +27,6 @@ public class AuthenticationController {
     public ResponseEntity<AuthenticationResponse> authenticate(
             @RequestBody AuthenticationRequest request
     ) {
-
+        return ResponseEntity.ok(service.authenticate(request)); 
     }
 }
