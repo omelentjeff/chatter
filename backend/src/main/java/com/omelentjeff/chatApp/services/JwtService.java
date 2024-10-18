a21458a23dfe6f5c27e732122412883a156f6baa
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-10-18 12:11:28 +0300

    BACKEND fix: make secret key be 256 bits, just a placeholder for now

--- backend/src/main/java/com/omelentjeff/chatApp/services/JwtService.java
+++ backend/src/main/java/com/omelentjeff/chatApp/services/JwtService.java
@@ -16,7 +16,7 @@ import java.util.function.Function;
 @Service
 public class JwtService {
 
-    private static final String SECRET_KEY = "SECRET";
+    private static final String SECRET_KEY = "ffd4b36b298ea10c4da4ba32fa9a63b1fc8f63fda2fa271b43e947ebb9c5d9adb4848a7c4e7faade8671d7b3f5edb658d9714a9127a82848839f9af207d9b331f9f06237df53b76413e4f26b0c815d0d9e4035d339fd0802be282358591c8fad5471b2cb1ac208ca46f4c9c0bf9578648ebc5dd45d3dbf523bcf589ff10b8f68ad90db16e09fb3c12d505a599506b2c99452d4098df4423b281feafb434ccda3ba3af51082a5d28226b52595e1cc166ec243dc092170c301c37e4106bf9865c702976985bfa6a8691804d6dfcc255921354dd65b1193e2cb07463f6788780846d9b28bfbdcb68f5db4b9584a5bc89fb7034c4f8efdbe713936ec8732d31fcda4";
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
     }
