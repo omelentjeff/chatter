c995de1c125751b0a6c3aaf37a275c30499a049f
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-12-05 12:35:18 +0200

    backend CREATE: GlobalExceptionHandler class

---
+++ backend/src/main/java/com/omelentjeff/chatApp/exception/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.omelentjeff.chatApp.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UsernameAlreadyInUseException.class)
+    public ResponseEntity<ErrorResponse> handleUsernameInUse(UsernameAlreadyInUseException ex) {
+        var error = ErrorResponse
+                .builder()
+                .status(HttpStatus.BAD_REQUEST.value())
+                .message(ex.getMessage())
+                .timeStamp(System.currentTimeMillis())
+                .build();
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+}
