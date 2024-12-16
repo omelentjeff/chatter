c5aee5abe14623fe1c88a9a37dfe689aec07ee16
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-12-16 19:11:57 +0200

    frontend FEAT: add Home page to router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./components/LoginPage";
 import Signup from "./components/Signup";
+import Home from "./components/Home";
 import { AuthProvider } from "./hooks/AuthProvider";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<LoginPage />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/home" element={<Home />} />
         </Routes>
       </AuthProvider>
     </Router>
