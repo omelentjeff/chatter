f0a3a095d85d152f9a95cd19d2fdb55f9eb1ecdd
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-12-16 18:05:27 +0200

    backend FEAT: add global cors settings

---
+++ backend/src/main/java/com/omelentjeff/chatApp/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.omelentjeff.chatApp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+@EnableWebSecurity
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000", "http://localhost:5173", "http://localhost:8080")
+                .allowedMethods("GET", "POST", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
