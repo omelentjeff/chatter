c7415acdaff3162e379864650368fe00667df7da
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-12-16 19:03:01 +0200

    frontend FEAT: add register page to router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./components/LoginPage";
+import Signup from "./components/Signup";
 import { AuthProvider } from "./hooks/AuthProvider";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <AuthProvider>
         <Routes>
           <Route path="/" element={<LoginPage />} />
+          <Route path="/signup" element={<Signup />} />
         </Routes>
       </AuthProvider>
     </Router>
