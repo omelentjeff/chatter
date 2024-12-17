9fabeecee227933481e0913d69c1408ff8e26578
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-12-17 12:24:39 +0200

    frontend CREATE: app bar template to include logout functionality

---
+++ frontend/src/components/MyAppBar.jsx
@@ -0,0 +1,38 @@
+import * as React from "react";
+import { Link as RouterLink, useNavigate } from "react-router-dom";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import { Typography, Button } from "@mui/material";
+import { useAuth } from "../hooks/AuthProvider";
+
+export default function MyAppBar() {
+  const navigate = useNavigate();
+  const { logout } = useAuth();
+  const { token } = useAuth();
+
+  const handleLogout = () => {
+    logout();
+  };
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, textAlign: "center" }}
+          >
+            Chatter
+          </Typography>
+          {token && (
+            <Button color="inherit" onClick={handleLogout}>
+              Logout
+            </Button>
+          )}
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
