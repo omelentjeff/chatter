a5d7b692e1ac02dde8e7a1428251f9bed2795de1
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-12-17 12:24:58 +0200

    frontend FEAT: only display app bar to authenticated users

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,33 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./components/LoginPage";
 import Signup from "./components/Signup";
+import MyAppBar from "./components/MyAppBar";
 import Home from "./components/Home";
-import { AuthProvider } from "./hooks/AuthProvider";
+import { AuthProvider, useAuth } from "./hooks/AuthProvider";
 
 function App() {
   return (
     <Router>
       <AuthProvider>
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/home" element={<Home />} />
-        </Routes>
+        <AppLayout />
       </AuthProvider>
     </Router>
+  );
+}
+
+function AppLayout() {
+  const { token } = useAuth(); // Assume `user` is null when not authenticated
+  console.log("User", token);
+
+  return (
+    <>
+      {token && <MyAppBar />} {/* Show AppBar only if user is authenticated */}
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </>
   );
 }
 
