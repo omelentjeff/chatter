00083a807092c9890ff55f83baaef0389e04b6d1
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-12-19 18:59:50 +0200

    backend FEAT: implement controller method to save new chat

--- backend/src/main/java/com/omelentjeff/chatApp/controllers/ChatController.java
+++ backend/src/main/java/com/omelentjeff/chatApp/controllers/ChatController.java
@@ -1,7 +1,11 @@
 package com.omelentjeff.chatApp.controllers;
 
+import com.omelentjeff.chatApp.models.Chat;
 import com.omelentjeff.chatApp.services.ChatService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class ChatController {
 
     private final ChatService chatService;
+
+    @PostMapping
+    public ResponseEntity<Chat> createChat(@RequestBody Chat chat) {
+        Chat savedChat = chatService.save(chat);
+        return ResponseEntity.ok(savedChat);
+    }
 }
