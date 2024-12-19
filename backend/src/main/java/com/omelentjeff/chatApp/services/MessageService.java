d79873c6e1e88117c95e4d99975f287bc65ebc1e
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-12-19 18:40:08 +0200

    backend CREATE: MessageService, autowire MessageRepository

---
+++ backend/src/main/java/com/omelentjeff/chatApp/services/MessageService.java
@@ -0,0 +1,11 @@
+package com.omelentjeff.chatApp.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MessageService {
+
+    private final MessageService messageService;
+}
