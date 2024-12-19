9cbb215396b8e5c5c7a8b0733ba74e4ab274e343
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-12-19 18:37:45 +0200

    backend CREATE: MessageRepository

---
+++ backend/src/main/java/com/omelentjeff/chatApp/repositories/MessageRepository.java
@@ -0,0 +1,9 @@
+package com.omelentjeff.chatApp.repositories;
+
+import com.omelentjeff.chatApp.models.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MessageRepository extends JpaRepository<Message, Long> {
+}
