947b01cd6651d4940bd9426e8969e68f4fd93700
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-12-19 17:50:49 +0200

    backend FEAT: override configureMessageBroker method

--- backend/src/main/java/com/omelentjeff/chatApp/config/WebSocketConfig.java
+++ backend/src/main/java/com/omelentjeff/chatApp/config/WebSocketConfig.java
@@ -1,4 +1,19 @@
 package com.omelentjeff.chatApp.config;
 
-public class WebSocketConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/user");
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.setUserDestinationPrefix("/user");
+    }
 }
