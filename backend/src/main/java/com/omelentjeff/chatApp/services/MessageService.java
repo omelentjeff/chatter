d022472a0e8a2d2ae7d7e7a0b05a2d80fc70e8a1
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-12-19 18:46:04 +0200

    backend FEAT: implement a service method to find all messages by chat id

--- backend/src/main/java/com/omelentjeff/chatApp/services/MessageService.java
+++ backend/src/main/java/com/omelentjeff/chatApp/services/MessageService.java
@@ -1,11 +1,19 @@
 package com.omelentjeff.chatApp.services;
 
+import com.omelentjeff.chatApp.models.Message;
+import com.omelentjeff.chatApp.repositories.MessageRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class MessageService {
 
-    private final MessageService messageService;
+    private final MessageRepository messageRepository;
+
+    public List<Message> findByChatId(Long chatId) {
+        return messageRepository.findByChatChatId(chatId);
+    }
 }
