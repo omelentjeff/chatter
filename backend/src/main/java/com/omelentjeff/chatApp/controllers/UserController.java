ba89986702429d5f0d4a6576ab93dc6416681c79
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-12-19 18:56:11 +0200

    backend CREATE: UserController, autowire UserService

---
+++ backend/src/main/java/com/omelentjeff/chatApp/controllers/UserController.java
@@ -0,0 +1,14 @@
+package com.omelentjeff.chatApp.controllers;
+
+import com.omelentjeff.chatApp.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+}
