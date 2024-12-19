ba77895c220ec65de6ee5d33d0252dc9352ee70a
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-12-19 18:54:57 +0200

    backend CREATE: ChatController, autowire ChatService

---
+++ backend/src/main/java/com/omelentjeff/chatApp/controllers/ChatController.java
@@ -0,0 +1,14 @@
+package com.omelentjeff.chatApp.controllers;
+
+import com.omelentjeff.chatApp.services.ChatService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/chats")
+public class ChatController {
+
+    private final ChatService chatService;
+}
