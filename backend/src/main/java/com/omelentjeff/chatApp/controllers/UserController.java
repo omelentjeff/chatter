201d30938410dddcf1039d16238d98be8b48f142
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-12-19 18:57:58 +0200

    backend FEAT: implement controller method to fetch all users

--- backend/src/main/java/com/omelentjeff/chatApp/controllers/UserController.java
+++ backend/src/main/java/com/omelentjeff/chatApp/controllers/UserController.java
@@ -1,9 +1,14 @@
 package com.omelentjeff.chatApp.controllers;
 
+import com.omelentjeff.chatApp.models.UserEntity;
 import com.omelentjeff.chatApp.services.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -11,4 +16,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
+
+    @GetMapping("/")
+    public ResponseEntity<List<UserEntity>> getAllUsers() {
+        List<UserEntity> users = userService.findAllUsers();
+        return ResponseEntity.ok(users);
+    }
 }
