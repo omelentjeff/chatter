b49374b1d4519ab20bc62368c89edcbd766eb4a6
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-12-19 18:21:53 +0200

    backend FEAT: create a service method to find all users

--- backend/src/main/java/com/omelentjeff/chatApp/services/UserService.java
+++ backend/src/main/java/com/omelentjeff/chatApp/services/UserService.java
@@ -1,7 +1,19 @@
 package com.omelentjeff.chatApp.services;
 
+import com.omelentjeff.chatApp.models.UserEntity;
+import com.omelentjeff.chatApp.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public List<UserEntity> findAllUsers() {
+        return userRepository.findAll();
+    }
 }
