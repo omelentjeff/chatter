0961aeee93a5d72871739496c2a7877f17cda594
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-12-20 20:55:02 +0200

    backend FEAT: implement user and chat association upon saving a new chat

--- backend/src/main/java/com/omelentjeff/chatApp/services/ChatService.java
+++ backend/src/main/java/com/omelentjeff/chatApp/services/ChatService.java
@@ -1,17 +1,37 @@
 package com.omelentjeff.chatApp.services;
 
 import com.omelentjeff.chatApp.models.Chat;
+import com.omelentjeff.chatApp.models.UserChat;
+import com.omelentjeff.chatApp.models.UserEntity;
 import com.omelentjeff.chatApp.repositories.ChatRepository;
+import com.omelentjeff.chatApp.repositories.UserChatRepository;
+import com.omelentjeff.chatApp.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ChatService {
 
     private final ChatRepository chatRepository;
+    private final UserRepository userRepository;
+    private final UserChatRepository userChatRepository;
 
-    public Chat save(Chat chat) {
-        return chatRepository.save(chat);
+    public Chat save(Chat chat, List<Integer> userIds) {
+
+        Chat savedChat = chatRepository.save(chat);
+
+        List<UserEntity> users = userRepository.findAllById(userIds);
+
+        for (UserEntity user : users) {
+            UserChat userChat = new UserChat();
+            userChat.setChat(savedChat);
+            userChat.setUser(user);
+            userChatRepository.save(userChat);
+        }
+
+        return savedChat;
     }
 }
