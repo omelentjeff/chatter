e031b0b02cce0a43043fd1fcfd9c6ba0846578fc
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-12-21 11:24:33 +0200

    backend FEAT: implement MessageDTO

---
+++ backend/src/main/java/com/omelentjeff/chatApp/dto/MessageDTO.java
@@ -0,0 +1,21 @@
+package com.omelentjeff.chatApp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@Builder
+public class MessageDTO {
+    private Long messageId;
+    private UserDTO sender;
+    private UserDTO recipient;
+    private String content;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
+
