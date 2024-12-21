095669b5542e476b3f507e26d2c36ef2c59280f0
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-12-21 17:49:48 +0200

    backend FEAT: implement service method to find all user's chats

--- backend/src/main/java/com/omelentjeff/chatApp/services/UserChatService.java
+++ backend/src/main/java/com/omelentjeff/chatApp/services/UserChatService.java
@@ -1,19 +1,25 @@
 package com.omelentjeff.chatApp.services;
 
+import com.omelentjeff.chatApp.dto.ChatDTO;
+import com.omelentjeff.chatApp.mapper.ChatMapper;
 import com.omelentjeff.chatApp.models.Chat;
 import com.omelentjeff.chatApp.repositories.UserChatRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class UserChatService {
 
     private final UserChatRepository userChatRepository;
+    private final ChatMapper chatMapper;
 
-    public List<Chat> getChatsById(Integer id) {
-        return userChatRepository.findChatsByUserId(id);
+    public List<ChatDTO> getChatsById(Integer id) {
+        List<Chat> chats = userChatRepository.findChatsByUserId(id);
+
+        return chats.stream().map(chatMapper::toChatDTO).toList();
     }
 }
--- backend/src/main/java/com/omelentjeff/chatApp/services/UserService.java
+++ backend/src/main/java/com/omelentjeff/chatApp/services/UserService.java
@@ -1,6 +1,9 @@
 package com.omelentjeff.chatApp.services;
 
+import com.omelentjeff.chatApp.dto.ChatDTO;
+import com.omelentjeff.chatApp.dto.UserDTO;
 import com.omelentjeff.chatApp.models.UserEntity;
+import com.omelentjeff.chatApp.repositories.UserChatRepository;
 import com.omelentjeff.chatApp.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,8 +15,13 @@ import java.util.List;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final UserChatService userChatService;
 
     public List<UserEntity> findAllUsers() {
         return userRepository.findAll();
+    }
+
+    public List<ChatDTO> findAllChatsById(Integer id) {
+        return userChatService.getChatsById(id);
     }
 }
