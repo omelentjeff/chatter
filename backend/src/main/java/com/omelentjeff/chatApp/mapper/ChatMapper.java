f75a767fad1d4d7d961b687eccc59d039e491eef
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-12-21 17:59:23 +0200

    backend FEAT: implement a custom mapper method to include user ids in ChatDTO object

--- backend/src/main/java/com/omelentjeff/chatApp/mapper/ChatMapper.java
+++ backend/src/main/java/com/omelentjeff/chatApp/mapper/ChatMapper.java
@@ -2,12 +2,27 @@ package com.omelentjeff.chatApp.mapper;
 
 import com.omelentjeff.chatApp.dto.ChatDTO;
 import com.omelentjeff.chatApp.models.Chat;
+import com.omelentjeff.chatApp.models.UserChat;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public interface ChatMapper {
 
+    @Mapping(target = "userIds", source = "userChats", qualifiedByName = "mapUserIds")
     ChatDTO toChatDTO(Chat chat);
 
     Chat toEntity(ChatDTO chatDTO);
+
+    @Named("mapUserIds")
+    default List<Integer> mapUserIds(List<UserChat> userChats) {
+        return userChats.stream()
+                .map(userChat -> userChat.getUser().getId())
+                .collect(Collectors.toList());
+    }
+
 }
