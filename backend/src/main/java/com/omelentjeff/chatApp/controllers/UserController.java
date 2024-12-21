f46b37066d148368464e9fddc9a1362adcc15c11
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-12-21 17:59:57 +0200

    backend FEAT: implement controller method to find all chats by user's id

--- backend/src/main/java/com/omelentjeff/chatApp/controllers/UserController.java
+++ backend/src/main/java/com/omelentjeff/chatApp/controllers/UserController.java
@@ -1,10 +1,13 @@
 package com.omelentjeff.chatApp.controllers;
 
+import com.omelentjeff.chatApp.dto.ChatDTO;
+import com.omelentjeff.chatApp.models.Chat;
 import com.omelentjeff.chatApp.models.UserEntity;
 import com.omelentjeff.chatApp.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +24,11 @@ public class UserController {
     public ResponseEntity<List<UserEntity>> getAllUsers() {
         List<UserEntity> users = userService.findAllUsers();
         return ResponseEntity.ok(users);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<List<ChatDTO>> findAllById(@PathVariable Integer id) {
+        List<ChatDTO> chats = userService.findAllChatsById(id);
+        return ResponseEntity.ok(chats);
     }
 }
