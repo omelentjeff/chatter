6fd0434dae6620e5b5c737f2c1c79e2e2e291364
Otto Melentjeff <otto.melentjeff@tuni.fi>
2025-01-29 21:01:06 +0200

    Dockerize the app

--- backend/src/main/java/com/omelentjeff/chatApp/config/WebSocketConfig.java
+++ backend/src/main/java/com/omelentjeff/chatApp/config/WebSocketConfig.java
@@ -20,7 +20,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:5173", "http://localhost:8080")
+                .setAllowedOrigins("http://localhost:5173", "http://localhost:8080", "http://localhost:3000")
                 .withSockJS();
     }
 }
---
+++ db-init-scripts/db-init.sql
@@ -0,0 +1,47 @@
+CREATE TABLE user_entity (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(255),
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(50) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE chat (
+    chat_id SERIAL PRIMARY KEY,
+    chat_name VARCHAR(100),
+    is_group BOOLEAN NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE user_chat (
+    user_chat_id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    chat_id INT NOT NULL,
+    joined_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    FOREIGN KEY (user_id) REFERENCES user_entity (id) ON DELETE CASCADE,
+    FOREIGN KEY (chat_id) REFERENCES chat (chat_id) ON DELETE CASCADE
+);
+
+CREATE TABLE message (
+    message_id SERIAL PRIMARY KEY,
+    chat_id INT NOT NULL,
+    sender_id INT NOT NULL,
+    content TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    is_deleted BOOLEAN DEFAULT FALSE,
+    read_status BOOLEAN NOT NULL,
+    FOREIGN KEY (chat_id) REFERENCES chat (chat_id) ON DELETE CASCADE,
+    FOREIGN KEY (sender_id) REFERENCES user_entity (id) ON DELETE CASCADE
+);
+
+CREATE TABLE message_read_status (
+    id SERIAL PRIMARY KEY,
+    message_id INT NOT NULL,
+    user_id INT NOT NULL,
+    is_read BOOLEAN NOT NULL,
+    FOREIGN KEY (message_id) REFERENCES message (message_id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES user_entity (id) ON DELETE CASCADE
+);
